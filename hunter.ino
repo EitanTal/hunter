c869d6e3ff28ec733307a2096961a720d87aee75
eitantal777 <eitantal777@gmail.com>
2021-09-21 21:02:18 -0600

    integration

--- hunter.ino
+++ hunter.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 //#include "LowPower.h"
 //#define USE_SERIAL
+#include "CC1101.h"
 
 enum 
 {
@@ -26,6 +27,12 @@ enum
 uint64_t tx_out = 0;
 static int tx_bit_offset = 0;
 
+//////////////////////////////////////////////////////
+
+void send_data(uint8_t* le_data, int loops);
+
+////////////////////////////////////////////////////////
+
 void setup() 
 {
 #ifdef USE_SERIAL
@@ -44,6 +51,8 @@ void setup()
   digitalWrite(BUTTON_4, HIGH);
   digitalWrite(BUTTON_5, HIGH);
   digitalWrite(BUTTON_6, HIGH);
+
+  cc_setup();
 }
 
 void my_tx(uint8_t a, int bits)
@@ -113,6 +122,87 @@ void loop()
 #else
     digitalWrite(LED_ACTIVITY, HIGH);
 #endif
-    delay(200); // ! replace with a loop to transmit the code for long enough time.
+    //delay(200); // ! replace with a loop to transmit the code for long enough time.
+    send_data(tx_buf, 100);
   }
+}
+
+/////////////////////////////////////////////
+
+
+CC1101 cc1101;
+
+#define LEDOUTPUT 7
+
+void blinker()
+{
+    static int i = 0;
+    digitalWrite(LEDOUTPUT, i ? HIGH : LOW);
+    i = !i;
+}
+
+void cc_setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting CC1101...");
+
+    // blink once to signal the setup
+    blinker();
+
+    Serial.println("Initialize and set registers.");
+
+    cc1101.init();
+
+    Serial.println("Setting PA_TABLE.");
+    config2();
+
+    uint8_t syncWord[] = {0x55, 0x55};
+    cc1101.setSyncWord(syncWord, false);
+    cc1101.setCarrierFreq(CFREQ_302);
+    cc1101.disableAddressCheck();
+
+    delay(1000);
+
+    Serial.print("CC1101_PARTNUM ");
+    Serial.println(cc1101.readReg(CC1101_PARTNUM, CC1101_STATUS_REGISTER));
+    Serial.print("CC1101_VERSION ");
+    Serial.println(cc1101.readReg(CC1101_VERSION, CC1101_STATUS_REGISTER));
+    Serial.print("CC1101_MARCSTATE ");
+    Serial.println(cc1101.readReg(CC1101_MARCSTATE, CC1101_STATUS_REGISTER) & 0x1f);
+
+    Serial.println("device initialized");
+}
+
+void send_data(uint8_t* le_data, int loops)
+{
+    CCPACKET data;
+
+    data.length = 5;
+    uint8_t known_good_data[] = {0b00101101, 0b10110110, 0b01001011, 0b00100100, 0b10010010};
+
+    memset(data.data, 0, sizeof(data.data));
+    memcpy(data.data, known_good_data, sizeof(data.length));
+
+    for (int i = 0; i < loops; i++)
+    {
+      blinker();
+      if (!cc1101.sendData(data))
+      {
+          Serial.println("sent failed :(");
+      }
+      delay(10);
+    }
+}
+
+void config2()
+{
+    enum
+    {
+        PA_POWER_MINUS_0  = 0x51,
+        PA_POWER_MINUS_15 = 0x1D,
+        PA_POWER_MINUS_30 = 0x03,
+        PA_POWER_ORIGINAL = 0xC0,
+    };
+    uint8_t PA_TABLE[] = {0x00, PA_POWER_MINUS_0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    cc1101.writeBurstReg(CC1101_PATABLE, PA_TABLE, 8);
 }
