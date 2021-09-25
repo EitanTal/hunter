c964dd6ff6d9a1d80f004262b02f41c3af63bfd1
eitantal777 <eitantal777@gmail.com>
2021-09-24 21:12:03 -0600

    consolidation cont'd

--- hunter.ino
+++ hunter.ino
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-//#include "LowPower.h"
-//#define USE_SERIAL
 #include "CC1101.h"
 #include "hunter_data.h"
 
@@ -15,17 +13,12 @@ enum
   LED_ACTIVITY = 7
 };
 
-//////////////////////////////////////////////////////
-
-void cc_send_data(uint8_t *le_data, int loops);
+CC1101 cc1101;
 
 ////////////////////////////////////////////////////////
 
 void setup()
 {
-#ifdef USE_SERIAL
-  Serial.begin(9600);
-#endif
   pinMode(LED_ACTIVITY, OUTPUT);
   pinMode(BUTTON_1, INPUT);
   pinMode(BUTTON_2, INPUT);
@@ -40,7 +33,8 @@ void setup()
   digitalWrite(BUTTON_5, HIGH);
   digitalWrite(BUTTON_6, HIGH);
 
-  cc_setup();
+  cc1101.init();
+  delay(1000);
 }
 
 void loop()
@@ -64,85 +58,12 @@ void loop()
     hunter_data_add_bits(action, 8);
     uint8_t *tx_buf = hunter_data_get_buffer();
 
-#ifdef USE_SERIAL
-    char out_text[8] = {0};
-    for (int i = 0; i < SIZEOF_HUNTER_DATA_BUFFER; i++)
-    {
-      sprintf(out_text, "0x%02X ", tx_buf[i]);
-      Serial.print(out_text);
-    }
-    Serial.print("\n");
-#else
     digitalWrite(LED_ACTIVITY, HIGH);
-#endif
-
-    cc_send_data(tx_buf, 16);
-  }
-}
-
-/////////////////////////////////////////////
-
-CC1101 cc1101;
-
-#define LEDOUTPUT 7
-
-void blinker()
-{
-  static int i = 0;
-  digitalWrite(LEDOUTPUT, i ? HIGH : LOW);
-  i = !i;
-}
-
-void cc_setup()
-{
-#ifdef USE_SERIAL
-  Serial.begin(9600);
-  Serial.println("Starting CC1101...");
-#endif
-  // blink once to signal the setup
-  blinker();
-#ifdef USE_SERIAL
-  Serial.println("Initialize and set registers.");
-#endif
-  cc1101.init();
-#ifdef USE_SERIAL
-  Serial.println("Setting PA_TABLE.");
-#endif
-  //config2(); // set PA Table here
-#if 0
-  uint8_t syncWord[] = {0x55, 0x55};
-  cc1101.setSyncWord(syncWord, false);
-  cc1101.disableAddressCheck();
-#endif
-  delay(1000);
-#ifdef USE_SERIAL
-  Serial.print("CC1101_PARTNUM ");
-  Serial.println(cc1101.readReg(CC1101_PARTNUM, CC1101_STATUS_REGISTER));
-  Serial.print("CC1101_VERSION ");
-  Serial.println(cc1101.readReg(CC1101_VERSION, CC1101_STATUS_REGISTER));
-  Serial.print("CC1101_MARCSTATE ");
-  Serial.println(cc1101.readReg(CC1101_MARCSTATE, CC1101_STATUS_REGISTER) & 0x1f);
-
-  Serial.println("device initialized");
-#endif
-}
-
-void cc_send_data(uint8_t *le_data, int loops)
-{
-  uint8_t data[SIZEOF_HUNTER_DATA_BUFFER];
-
-  memcpy(data, le_data, sizeof(data));
-
-  for (int i = 0; i < loops; i++)
-  {
-    blinker();
-    if (!cc1101.sendData(data, SIZEOF_HUNTER_DATA_BUFFER))
+    for (int i = 0; i < 16; i++)
     {
-#ifdef USE_SERIAL
-      Serial.println("sent failed :(");
-#endif
+      digitalWrite(LED_ACTIVITY, ((i%2) ? HIGH : LOW));
+      cc1101.sendData(tx_buf, SIZEOF_HUNTER_DATA_BUFFER);
+      delay(10);
     }
-    delay(10);
   }
 }
-
