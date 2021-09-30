7067dedae2ffcd8b14e92c6b81f4d57fb08362a9
eitantal777 <eitantal777@gmail.com>
2021-09-30 17:59:38 -0600

    io.c initial implementation

--- io.c
+++ io.c
@@ -1,8 +1,26 @@
 #include "stm8l10x.h"
+#include "io.h"
+
+uint8_t arduinopin2pin[] =
+{
+    1, 1, 1, 1,
+    1, 1, 1, 1,
+    1, 1, 1, 1,
+    1, 1, 1, 1
+};
+
+void* arduinopin2port[] =
+{
+    GPIOA, GPIOA, GPIOA, GPIOA,
+    GPIOA, GPIOA, GPIOA, GPIOA,
+    GPIOA, GPIOA, GPIOA, GPIOA,
+    GPIOA, GPIOA, GPIOA, GPIOA,
+};
 
 void pinMode(int pin, int val)
 {
-
+    int mode = (val == OUTPUT) ? GPIO_Mode_Out_PP_High_Fast : GPIO_Mode_In_FL_No_IT;
+    GPIO_Init(arduinopin2port[pin], 1 << arduinopin2pin[pin], mode);
 }
 
 void digitalWrite(int pin, int val)
