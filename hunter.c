2b35bdc2522ecea72671e742687df602e6b6e69c
Eitan Tal <eitantal777@gmail.com>
2021-11-13 13:11:19 -0700

    Full Functionality (#7)

--- hunter.c
+++ hunter.c
@@ -4,74 +4,165 @@
 #include "io.h"
 #include "time.h"
 #include "stm8l10x_clk.h"
+#include "stm8l10x_exti.h"
 
 enum
 {
-  BUTTON_1 = PIN_Z0,
-  BUTTON_2 = PIN_Z1,
-  BUTTON_3 = PIN_A0,
-  BUTTON_4 = 3,
-  BUTTON_5 = 5,
-  BUTTON_6 = 9
+  BUTTON_LIGHT = PIN_Z0,
+  BUTTON_LIGHT_UP = PIN_Z1,
+  BUTTON_LIGHT_DOWN = PIN_A0,
+  BUTTON_FAN = 3,
+  BUTTON_FAN_UP = 5,
+  BUTTON_FAN_DOWN = 9
 };
 
 enum
 {
 	MY_LED_11 = 2, // Arduino D2 equivalent
-	MY_LED_2  = 6,  // Arduino D6 equivalent
-  LED_ACTIVITY = MY_LED_11
+	MY_LED_2  = 6  // Arduino D6 equivalent
+};
+
+enum
+{
+  COMMAND_ERROR,
+  COMMAND_OK
 };
 
 ////////////////////////////////////////////////////////
 
+
+static void WFI(void)
+{
+	__asm("WFI");
+}
+
+static void ENABLE_INTERRUPTS(void)
+{
+	__asm("RIM");
+}
+
 void setup(void)
 {
   CLK_PeripheralClockConfig(CLK_Peripheral_SPI, ENABLE);
-
+  
 	pinMode(MY_LED_11, OUTPUT);
 	pinMode(MY_LED_2, OUTPUT);
 
-  pinMode(BUTTON_1, INPUT_PULLUP);
-  pinMode(BUTTON_2, INPUT_PULLUP);
-  pinMode(BUTTON_3, INPUT_PULLUP);
-  pinMode(BUTTON_4, INPUT_PULLUP);
-  pinMode(BUTTON_5, INPUT_PULLUP);
-  pinMode(BUTTON_6, INPUT_PULLUP);
+  pinMode(BUTTON_LIGHT, INPUT_PULLUP);
+  pinMode(BUTTON_LIGHT_UP, INPUT_PULLUP);
+  pinMode(BUTTON_LIGHT_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_FAN, INPUT_PULLUP);
+  pinMode(BUTTON_FAN_UP, INPUT_PULLUP);
+  pinMode(BUTTON_FAN_DOWN, INPUT_PULLUP);
+
+  EXTI_SetPortSensitivity(EXTI_Port_B, EXTI_Trigger_Falling); // B2
+  EXTI_SetPinSensitivity(EXTI_Pin_0, EXTI_Trigger_Falling);   // B0
+  EXTI_SetPinSensitivity(EXTI_Pin_1, EXTI_Trigger_Falling);   // C1
+  EXTI_SetPinSensitivity(EXTI_Pin_2, EXTI_Trigger_Falling);   // C2
+  EXTI_SetPinSensitivity(EXTI_Pin_3, EXTI_Trigger_Falling);   // B3
+  EXTI_SetPinSensitivity(EXTI_Pin_4, EXTI_Trigger_Falling);   // C4
 
   cc1101_init();
 
   delay(1000);
+
+  ENABLE_INTERRUPTS();
 }
+
+void send_action(int action, int ok)
+{
+  uint8_t *tx_buf;
+  int i;
+  hunter_data_clear();
+  hunter_data_add_bits(0x0F, 5);
+  hunter_data_add_bits(action, 8);
+  tx_buf = hunter_data_get_buffer();
+
+  for (i = 0; i < 16; i++)
+  {
+    if (ok == COMMAND_OK)
+    {
+      digitalWrite(MY_LED_11, ((i%2) ? HIGH : LOW));
+      digitalWrite(MY_LED_2, ((i%2) ? HIGH : LOW));
+    }
+    else
+    {
+      digitalWrite(MY_LED_11, (((i/4)%2) ? HIGH : LOW));
+      digitalWrite(MY_LED_2, (((i/4)%2) ? LOW : HIGH));
+    }
+    cc1101_sendData(tx_buf, SIZEOF_HUNTER_DATA_BUFFER);
+    delay(10);
+  }
+}
+
+int speed2action(int8_t new_speed)
+{
+  switch (new_speed)
+  {
+    case 1:    return DATA_FAN_3;
+    case 2:    return DATA_FAN_2;
+    case 3:    return DATA_FAN_1;
+  }
+  return DATA_FAN_OFF;
+}
+
 
 void loop(void)
 {
-  int action = 0;
+  static int8_t current_fan_speed = 0;
 
-  digitalWrite(LED_ACTIVITY, LOW);
-  delay(10); // ! replace with low-power
+  digitalWrite(MY_LED_11, LOW);
+  digitalWrite(MY_LED_2, LOW);
+  delay(100);
+	WFI();
 
-  if (digitalRead(BUTTON_1) == LOW) action = DATA_LIGHT; 
-  if (digitalRead(BUTTON_2) == LOW) action = DATA_FAN_OFF;
-  if (digitalRead(BUTTON_3) == LOW) action = DATA_FAN_REVERSE;
-  //if (digitalRead(BUTTON_4) == LOW) action = DATA_FAN_3;
-  if (digitalRead(BUTTON_5) == LOW) action = DATA_FAN_2;
-  if (digitalRead(BUTTON_6) == LOW) action = DATA_FAN_1;
-
-  if (action != 0)
+  if (digitalRead(BUTTON_LIGHT) == LOW)
   {
-		uint8_t *tx_buf;
-		int i;
-    hunter_data_clear();
-    hunter_data_add_bits(0x0F, 5);
-    hunter_data_add_bits(action, 8);
-    tx_buf = hunter_data_get_buffer();
+    send_action(DATA_LIGHT, COMMAND_OK);
+  }
+#if 0 // No support for light-up / light-down
+  if (digitalRead(BUTTON_LIGHT_UP) == LOW) action = DATA_FAN_OFF;
+  if (digitalRead(BUTTON_LIGHT_DOWN) == LOW) action = DATA_FAN_REVERSE;
+#endif
 
-    digitalWrite(LED_ACTIVITY, HIGH);
-    for (i = 0; i < 16; i++)
+#if 0 // ! no pull-up resistor installed
+  if (digitalRead(BUTTON_FAN) == LOW)
+  {
+    current_fan_speed = 0;
+    send_action(DATA_FAN_OFF);
+  }
+#endif
+  if ((digitalRead(BUTTON_FAN_UP) == LOW) && (digitalRead(BUTTON_FAN_DOWN) == LOW))
+  {
+    send_action(DATA_FAN_REVERSE, COMMAND_OK);
+  }
+  else
+  {
+    if (digitalRead(BUTTON_FAN_UP) == LOW)
     {
-      digitalWrite(LED_ACTIVITY, ((i%2) ? HIGH : LOW));
-      cc1101_sendData(tx_buf, SIZEOF_HUNTER_DATA_BUFFER);
-      delay(10);
+      current_fan_speed++;
+      if (current_fan_speed > 3) 
+      {
+        current_fan_speed = 3;
+        send_action(speed2action(current_fan_speed), COMMAND_ERROR);
+      }
+      else
+      {
+        send_action(speed2action(current_fan_speed), COMMAND_OK);
+      }
     }
+    if (digitalRead(BUTTON_FAN_DOWN) == LOW)
+    {
+      current_fan_speed--;
+      if (current_fan_speed < 0)
+      {
+        current_fan_speed = 0;
+        send_action(speed2action(current_fan_speed), COMMAND_ERROR);
+      }
+      else
+      {
+        send_action(speed2action(current_fan_speed), COMMAND_OK);
+      }
+    } 
   }
 }
--- stm8_interrupt_vector.c
+++ stm8_interrupt_vector.c
@@ -2,6 +2,7 @@
  *	Copyright (c) 2007 STMicroelectronics
  */
 #include "time.h"
+#include "stm8l10x.h"
 
 typedef void @far (*interrupt_handler_t)(void);
 
@@ -9,6 +10,12 @@ struct interrupt_vector {
 	unsigned char interrupt_instruction;
 	interrupt_handler_t interrupt_handler;
 };
+
+void ack_all_exti(void)
+{
+	EXTI->SR1 = 0xFF;
+	EXTI->SR2 = 0xFF;
+}
 
 @far @interrupt void NonHandledInterrupt (void)
 {
@@ -18,9 +25,80 @@ struct interrupt_vector {
 	return;
 }
 
+
+@far @interrupt void IRQ0 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ1 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ2 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ3 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ4 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ5 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ6 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ7 (void)
+{
+	return;
+}
+
+@far @interrupt void IRQ8 (void)
+{
+	ack_all_exti(); // light down
+	return;
+}
+
+@far @interrupt void IRQ9 (void)
+{
+	ack_all_exti();
+	return;
+}
+
+@far @interrupt void IRQ10(void)
+{
+	ack_all_exti();
+	return;
+}
+
+@far @interrupt void IRQ11(void)
+{
+	ack_all_exti(); // light up
+	return;
+}
+
+@far @interrupt void IRQ12(void)
+{
+	ack_all_exti();
+	return;
+}
+
 @far @interrupt void IRQ13 (void)
 {
-	//ISR_TIM2_UPDATEOVERFLOW(); // ! Example
+	return;
 }
 
 extern void _stext();     /* startup routine */
@@ -29,19 +107,19 @@ extern void _stext();     /* startup routine */
 struct interrupt_vector const _vectab[] = {
 	{0x82, (interrupt_handler_t)_stext}, /* reset */
 	{0x82, NonHandledInterrupt}, /* trap  */
-	{0x82, NonHandledInterrupt}, /* irq0  */
-	{0x82, NonHandledInterrupt}, /* irq1  */
-	{0x82, NonHandledInterrupt}, /* irq2  */
-	{0x82, NonHandledInterrupt}, /* irq3  */
-	{0x82, NonHandledInterrupt}, /* irq4  */
-	{0x82, NonHandledInterrupt}, /* irq5  */
-	{0x82, NonHandledInterrupt}, /* irq6  */
-	{0x82, NonHandledInterrupt}, /* irq7  */
-	{0x82, NonHandledInterrupt}, /* irq8  */
-	{0x82, NonHandledInterrupt}, /* irq9  */
-	{0x82, NonHandledInterrupt}, /* irq10 */
-	{0x82, NonHandledInterrupt}, /* irq11 */
-	{0x82, NonHandledInterrupt}, /* irq12 */
+	{0x82, IRQ0}, /* irq0  */
+	{0x82, IRQ1}, /* irq1  */
+	{0x82, IRQ2}, /* irq2  */
+	{0x82, IRQ3}, /* irq3  */
+	{0x82, IRQ4}, /* irq4  */
+	{0x82, IRQ5}, /* irq5  */
+	{0x82, IRQ6}, /* irq6  */
+	{0x82, IRQ7}, /* irq7  */
+	{0x82, IRQ8}, /* irq8  */
+	{0x82, IRQ9}, /* irq9  */
+	{0x82, IRQ10}, /* irq10 */
+	{0x82, IRQ11}, /* irq11 */
+	{0x82, IRQ12}, /* irq12 */
 	{0x82, IRQ13}, /* irq13 */
 	{0x82, IRQ13}, /* irq14 */
 	{0x82, NonHandledInterrupt}, /* irq15 */
